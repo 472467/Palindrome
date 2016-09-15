4d00e39548e3d5c6b37c4c96928038027d1f5228
U-BSD\472467 <472467@SHS-10D8A6ABE.beaverton.k12.or.us>
2016-09-15 14:29:44 -0700

    changed to char arr, add some sorta working for loops, middle one not working yet

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,21 +1,38 @@
 /* Created by Brendan
-Checks if input is a palindrome, thats about it */
+Checks if input is a palindrome, thats about it 
+CYGWIN 64 BIT
+*/
 
 #include <iostream>
 #include <stdlib.h>
+#include <ctype.h>
 
 
 using namespace std;
 
 int main(){
-	string input, inputNoSpace;
-	cout >> "Input text to see if it is a palindrome, spaces do not matter.";
-	while
-	cin << input;
-	for(int x = 0; x < input.size(); x++){
-		if(!input.at(x).compare(' ')){
-			inputNoSpace+= input.at(x);
+	char input [80], inputNoSpace [80];
+	int openSpace = 0;
+	for(int x =0; x < 80; x++){
+		input[x]= '\0';
+		inputNoSpace[x]= '\0';
+	}
+	cout << "Input text to see if it is a palindrome, spaces or punctuation do not matter. ";
+	cin >> input;
+	
+	for(int x = 0; x < 80; x++){
+		if(!input[x] == ' ' && ispunct(input[x]) == 0){
+			cout << true;
+			inputNoSpace[openSpace]+= input[x];
+			openSpace++;
 		}
 	}
+	for(int x = 0; x < 80; x++){
+		cout << inputNoSpace[x];
+	}
+	//for(int x = 0;)
+	
+	
 	
 }
+
