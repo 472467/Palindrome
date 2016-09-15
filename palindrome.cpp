39326f8590c01cfd4d0c76e9ef425eca72d8f14f
U-BSD\472467 <472467@SHS-10D8A6ABE.beaverton.k12.or.us>
2016-09-15 13:30:16 -0700

    Basic stuff, before char arr

---
+++ palindrome.cpp
@@ -0,0 +1,21 @@
+/* Created by Brendan
+Checks if input is a palindrome, thats about it */
+
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int main(){
+	string input, inputNoSpace;
+	cout >> "Input text to see if it is a palindrome, spaces do not matter.";
+	while
+	cin << input;
+	for(int x = 0; x < input.size(); x++){
+		if(!input.at(x).compare(' ')){
+			inputNoSpace+= input.at(x);
+		}
+	}
+	
+}
