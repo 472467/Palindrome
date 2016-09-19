3b333d4eb0998b542254fe97715c59a87e752657
U-BSD\472467 <472467@SHS-10D8A6ABE.beaverton.k12.or.us>
2016-09-19 14:12:31 -0700

    added comments, whoops

--- palindrome.cpp
+++ palindrome.cpp
@@ -16,36 +16,37 @@ using namespace std;
 
 int main(){
 	char input [80], inputNoSpace [80], inputBackwards [80];
-	int openSpace = 0;
-	for(int x =0; x < 80; x++){
+	int openSpace = 0;//variable used to designate an open space for the text only input
+	for(int x =0; x < 80; x++){//cleans char arrays
 		input[x]= '\0';
 		inputNoSpace[x]= '\0';
+		inputBackwards[x]= '\0';
 	}
 	cout << "Input text to see if it is a palindrome, spaces or punctuation do not matter. \n";
-	cin.getline(input, 80);
+	cin.getline(input, 80);//cin alone skips whitespaces, getline fixes that
 	
-	for(int x = 0; x < 80; x++){
+	for(int x = 0; x < 80; x++){//removes punct and whitespace
 		if(isspace(input[x]) == 0 && ispunct(input[x]) == 0){
 			inputNoSpace[openSpace]+= input[x];
 			openSpace++;
 		}
 	}
-	int count = 0;
-	while(inputNoSpace[count] != '\0'){
+	int count = 0;//length of char array
+	while(inputNoSpace[count] != '\0'){//how long before it reaches end of line
 		count++;
 	}
-	int y = 0;
-	for(int x = count - 1; x >= 0; x--){
+	int y = 0;//counts up
+	for(int x = count - 1; x >= 0; x--){//starts from length of char array, -1 for null char
 		inputBackwards[y]= inputNoSpace[x];
 		y++;
 	}
-	inputBackwards[count]= '\0';
+	inputBackwards[count]= '\0';//explained in header
 	if(strcmp(inputNoSpace, inputBackwards) == 0){
 		cout << "\nIs palindrome.";
 	}else{
 		cout << "\nNot palindrome.";
 	}
 	
-	return 0;
+	return 0;//ends program with no errors
 }
 
